9f9946883ca7d876a1d84f88f5b29d5535b2deb4
gzj521 <751393193@qq.com>
2018-05-19 10:27:04 +0800

    login

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<form action="./logindo.php" method="post">
+			<table>
+				<tr>
+					<td>用户</td>
+					<td> <input type="text" name="name"></td>
+				</tr>
+				<tr>
+					<td>密码</td>
+					<td><input type="text" name="pwd"></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><input type="submit" value="登录"></td>
+				</tr>
+			</table>
+		</form>
+</body>
+</html>
